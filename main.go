e5ad92a0110baae59253a161549a593aa037b979
guilhermef.santos <guilhermef.santos@lince.com.br>
2023-11-11 14:30:32 -0300

    feat: repaginando ponteiro

--- main.go
+++ main.go
@@ -19,10 +19,22 @@ func main() {
 		numberAgency:  123456,
 		balance:       125.50,
 	}
+	account2 := CurrentAccount{
+		holder:        "Guilherme",
+		numberAccount: 589,
+		numberAgency:  123456,
+		balance:       125.50,
+	}
 	fmt.Println("Count of "+account.holder+"\nNumber agency:"+
 		"", account.numberAgency, "\nNumber account: ", account.numberAgency, ""+
 		"\nBalance:", account.balance)
 
+	fmt.Println("Count of "+account2.holder+"\nNumber agency:"+
+		"", account2.numberAgency, "\nNumber account: ", account2.numberAgency, ""+
+		"\nBalance:", account2.balance)
+
+	fmt.Println(account == account2)
+	fmt.Println("\n\n\n\n\n\n")
 	var countYasmin *CurrentAccount
 	countYasmin = new(CurrentAccount)
 	countYasmin.holder = "Yasmin"
@@ -30,4 +42,18 @@ func main() {
 
 	fmt.Println(*countYasmin, "Ponteiro")
 	fmt.Println(countYasmin, "Sem ponteiro")
+
+	var countYasmin2 *CurrentAccount
+	countYasmin2 = new(CurrentAccount)
+	countYasmin2.holder = "Yasmin"
+	countYasmin2.balance = 800
+
+	fmt.Println(*countYasmin2, "Ponteiro")
+	fmt.Println(countYasmin2, "Sem ponteiro\n\n")
+
+	fmt.Println(&countYasmin)
+	fmt.Println(&countYasmin2)
+	fmt.Println(countYasmin2, "Sem ponteiro")
+
+	fmt.Println(*countYasmin == *countYasmin2)
 }
